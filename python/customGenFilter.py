5281e2a883cb294503a7c2dbbbd9dc322f1cad95
Matthias Wolf <matthias@sushinara.net>
2016-11-08 08:05:32 -0500

    Add a genfilter customization with fake MVA setup.

--- python/customGenFilter.py
+++ python/customGenFilter.py
@@ -15,3 +15,22 @@ def customizeForGenFiltering(process):
     process.schedule.extend([process.ttHfilter_step])
     process.AODSIMoutput.SelectEvents.SelectEvents = cms.vstring('ttHfilter_step')
     return process
+
+
+def customizeForGenFilteringWithFakes(process):
+    process.load("ttH.TauMCGeneration.eventFilter_cfi")
+    process.ttHGenFilter.useFakeTaus = cms.bool(True)
+    process.ttHGenFilter.minTaus = cms.int32(2)
+    process.ttHGenFilter.fakeCut = cms.double(0.)
+    for path in process.paths:
+        if path in ['generation_step', 'lhe_step']:
+            continue
+        sequence = getattr(process, path)
+        if path in ['digitisation_step']:
+            sequence *= process.ttHGenFilter
+        else:
+            sequence.insert(1, process.ttHGenFilter)
+    process.ttHfilter_step = cms.Path(process.ttHGenFilter)
+    process.schedule.extend([process.ttHfilter_step])
+    process.AODSIMoutput.SelectEvents.SelectEvents = cms.vstring('ttHfilter_step')
+    return process

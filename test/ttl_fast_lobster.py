7de42b52985def9ba24cb167a54e6ba2def88e01
Matthias Wolf <matthias@sushinara.net>
2016-11-30 04:00:27 -0500

    Update Lobster configuration for LHE spin-off.

--- test/ttl_fast_lobster.py
+++ test/ttl_fast_lobster.py
@@ -1,42 +1,57 @@
-import glob
-
 from lobster.core import AdvancedOptions, Category, Config, StorageConfiguration, Workflow
 from lobster.core import ParentDataset, ProductionDataset
 
-version = 'v0'
+version = 'v1'
 
 storage = StorageConfiguration(
     output=[
         "hdfs://eddie.crc.nd.edu:19000/store/user/matze/ttH/fastsim_" + version,
         "file:///hadoop/store/user/matze/ttH/fastsim_" + version,
         "root://deepthought.crc.nd.edu//store/user/matze/ttH/fastsim_" + version,
-        "chirp://eddie.crc.nd.edu:9094/store/user/matze/ttH/fastsim_" + version,
         "gsiftp://T3_US_NotreDame/store/user/matze/ttH/fastsim_" + version,
+        "chirp://eddie.crc.nd.edu:9094/store/user/matze/ttH/fastsim_" + version,
         "srm://T3_US_NotreDame/store/user/matze/ttH/fastsim_" + version,
     ]
 )
 
-datasets = [s.replace('_aod.py', '') for s in glob.glob('*_aod.py') if 'filtered' not in s]
+datasets = ['fast_ttH', 'fast_ttjets_sl']
 workflows = []
 
 for dset in datasets:
-    aod = Workflow(
-        label=dset + '_AOD',
-        pset=dset + '_aod.py',
+    lhe = Workflow(
+        label=dset + '_lhe',
+        pset=dset + '_lhe.py',
         dataset=ProductionDataset(
-            events_per_task=1000,
+            events_per_task=50000,
             events_per_lumi=1000,
-            number_of_tasks=1000
+            number_of_tasks=100
         ),
         category=Category(
-            name='aod',
+            name='lhe',
             cores=1,
+            disk=2000,
             memory=2000
         )
     )
 
+    aod = Workflow(
+        label=dset + '_aod',
+        pset=dset + '_aod.py',
+        dataset=ParentDataset(
+            parent=lhe,
+            units_per_task=10
+        ),
+        category=Category(
+            name='aod',
+            cores=1,
+            disk=1000,
+            memory=2000,
+            runtime=90 * 60
+        )
+    )
+
     maod = Workflow(
-        label=dset + '_MiniAOD',
+        label=dset + '_maod',
         pset=dset + '_maod.py',
         merge_size='2000M',
         cleanup_input=True,
@@ -47,12 +62,13 @@ for dset in datasets:
         category=Category(
             name='maod',
             cores=1,
+            disk=4000,
             memory=2000,
-            runtime=45 * 60
+            runtime=90 * 60
         )
     )
 
-    workflows.extend([aod, maod])
+    workflows.extend([lhe, aod, maod])
 
 config = Config(
     workdir='/tmpscratch/users/matze/ttH/fastsim_' + version,

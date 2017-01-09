0492e87af145743bd842b2cfd9a5ce698218cff5
Matthias Wolf <matthias@sushinara.net>
2017-01-09 10:03:40 -0500

    Add ttZ AOD configuration.

---
+++ test/crab/ttZ_aod.py
@@ -0,0 +1,31 @@
+from WMCore.Configuration import Configuration
+
+sample = 'ttZ'
+step = 'aod'
+part = 'p1'
+
+nevents = int(25e6)
+njobevents = nevents / 10000
+
+config = Configuration()
+
+config.section_('General')
+config.General.requestName = '_'.join([sample, step, part])
+
+config.section_('JobType')
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'configs/{}_{}.py'.format(sample, step)
+config.JobType.maxMemoryMB = 4000
+config.JobType.numCores = 2
+
+config.section_('Data')
+config.Data.outputPrimaryDataset = 'TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = njobevents
+config.Data.totalUnits = nevents
+config.Data.publication = True
+config.Data.outputDatasetTag = '{}_v1{}'.format(step, part)
+
+config.section_('Site')
+config.Site.storageSite = 'T2_EE_Estonia'
+# config.Site.whitelist = ['T2_EE_Estonia']

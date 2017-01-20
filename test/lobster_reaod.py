46dd2948e88b5113a6a4a19354d625c9c633ca14
Matthias Wolf <matthias@sushinara.net>
2017-01-20 05:22:32 -0500

    Use the correct constructor keyword.

--- test/lobster_reaod.py
+++ test/lobster_reaod.py
@@ -41,7 +41,7 @@ for path in lhe:
         pset='configs/' + dset + '_aod.py',
         dataset=cmssw.Dataset(
             dataset=path,
-            instance='phys03',
+            dbs_instance='phys03',
             lumis_per_task=4
         ),
         category=Category(

6c2d7173818d0f25214675809bfb828790c46c9f
Matthias Wolf <matthias@sushinara.net>
2017-01-26 09:55:27 -0500

    Update example config for copying.

--- test/crab/copy.py
+++ test/crab/copy.py
@@ -9,7 +9,7 @@ def generate(dataset):
     config = Configuration()
 
     config.section_('General')
-    config.General.requestName = 'copy_' + tag
+    config.General.requestName = 'yet_another_copy_' + tag
 
     config.section_('JobType')
     config.JobType.pluginName = 'Analysis'
@@ -18,6 +18,9 @@ def generate(dataset):
     config.section_('Data')
     config.Data.inputDataset = dataset
     config.Data.inputDBS = 'phys03'
+    # ignoreLocality will work when running on an arbitrary site, as long
+    # as the files are present at a site that's hooked up in the federation
+    # (ND is not!)
     # config.Data.ignoreLocality = True
     config.Data.splitting = 'FileBased'
     config.Data.unitsPerJob = 4
@@ -25,11 +28,15 @@ def generate(dataset):
     config.Data.outputDatasetTag = 'copy_' + tag
 
     config.section_('Site')
+    # Apparently, we are blacklisted on the CRAB server side! Ignore the
+    # global blacklist...
+    config.Site.ignoreGlobalBlacklist = True
     # config.Site.whitelist = ['T3_US_NotreDame']
     config.Site.storageSite = 'T2_EE_Estonia'
     return config
 
-config = generate('/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/matze-faster_v5_ttH_maod_777fc0041cce4fac803f2dbb53a837f5-v1/USER')
+# config = generate('/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/matze-faster_v5_ttH_maod_777fc0041cce4fac803f2dbb53a837f5-v1/USER')
+config = generate('/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/matze-tau_v1_train_ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_v1_c0233c4da5eb4e858e2884f3ec6749dc-v1/USER')
 
 print("""
 Using the following configuration:
